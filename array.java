139f883cff86530be791c5e9e96410aab55a023b
wanjiku <wanjikukangangi@gmail.com>
2024-03-13 10:01:38 +0300

    array practice questions

--- array.java
+++ array.java
@@ -54,16 +54,80 @@ public class Arrayutil{
         return arr;
     }
 
+    // Find the minimum value of an array
+    public static int findMinimum(int[] arr){
+        // edge case
+        if (arr == null || arr.length == 0){
+            throw new IllegalArgumentException("Invalid input");
+        }
+        int min = arr[0];
+        for (int i=1; i < arr.length; i++){
+            if (arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    // Given an array of integers, return the second maximum value. The second maximu value exists
+
+    public static int findSecondMaximum(int[] arr){
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        for (int i=0; i < arr.length; i++){
+            if (arr[i] > max){
+                secondMax = max;
+                max = arr[i];
+            } else if(arr[i] > secondMax && arr[i] != max){
+                secondMax = arr[i];
+            }
+        }
+        return secondMax;
+    }
+
+    // Given an array of integers, write a function to move all 0's to end of it while maintaining tghe relative order of the non-zero elememts
+    // We use the two pointers
+
+    public static int[] moveZeros(int[] arr){
+        int j = 0;
+        for (int i= 0; i < arr.length; i++ ){
+            if(arr[i] != 0 && arr[j] == 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            if(arr[j] != 0){
+                j++;
+            }
+        }
+        return arr;
+    }
+
+
 
 
     public static void main(String[] args){
         Arrayutil arrUtil = new Arrayutil();
-        arrUtil.arrayDemo();
+        // Create array output
+        // arrUtil.arrayDemo();
+        // arrUtil.printArray( arrUtil.arrayDemo());
 
-        int[] arr = {3, 2, 4, 7, 10, 6, 5};
-        arrUtil.printArray(arrUtil.removeEven(arr));
 
-        System.out.println(arr.length - 1);
-        arrUtil.printArray(arrUtil.reverseArray(arr, 0, arr.length-1));
+        //int[] arr = {13, 12, 4, 17, 17, 8, 5};
+        // Remove even values demo
+        // arrUtil.printArray(arrUtil.removeEven(arr));
+
+        // Reverse array demo
+        // arrUtil.printArray(arrUtil.reverseArray(arr, 1, 5));
+
+        // Minimum value demo
+        // System.out.println(arrUtil.findMinimum((arr)));
+
+        // Second Maximum value demo
+        // System.out.println(arrUtil.findSecondMaximum(arr));
+
+        // Move zeros to end demo
+        int[] arr = {8, 2, 0, 7, 6, 0, 9, 5};
+        arrUtil.printArray( arrUtil.moveZeros(arr));
     }
 }
