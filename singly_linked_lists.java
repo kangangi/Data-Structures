9d789b1b34e846623d07a44c331c496813c2f304
wanjiku <wanjikukangangi@gmail.com>
2024-03-15 17:44:34 +0300

    singly linked list practice questions

--- singly_linked_lists.java
+++ singly_linked_lists.java
@@ -14,7 +14,7 @@ public class SinglyLinkedList{
     }
 
     // print out the list
-    public void display(){
+    public void display(ListNode head){
         ListNode current = head;
         while (current != null){
             System.out.print(current.data + " --> ");
@@ -55,8 +55,8 @@ public class SinglyLinkedList{
         current.next = newNode;
     }
 
-    // Implement method to inster a node at a given position.
-    public void instertPosition(int value, int position){
+    // Implement method to insert a node at a given position.
+    public void insertPosition(int value, int position){
         ListNode newNode = new ListNode(value);
         if (position == 1){
             newNode.next = head;
@@ -78,6 +78,85 @@ public class SinglyLinkedList{
 
     }
 
+    // Delete first node of a ListNode
+    public ListNode deleteFirst(){
+        if (head == null){
+            return null;
+        }
+        ListNode temp = head;
+        head = head.next;
+        temp.next = null;
+        return temp;
+    }
+
+    // Delete node from the end of the list
+    public ListNode deleteLast(){
+        if (head == null){
+            return null;
+        }
+        ListNode current = head;
+        ListNode previous = null;
+        while (current.next != null){
+            previous = current;
+            current = current.next;
+        }
+        previous.next = null;
+        return current;
+    }
+
+
+    // Deleteb node from a given position of list
+    public ListNode deleteNode(int position){
+        if (position == 1){
+            ListNode temp = head;
+            head = head.next;
+            return temp;
+        } else if (position == 0 || position > this.length() ){
+                System.out.println("Error, invalid position");
+                return null;
+        } else {
+            ListNode previous = head;
+            int count = 1;
+            while (count < position - 1){
+                previous = previous.next;
+                count++;
+            }
+            ListNode temp = previous.next;
+            previous.next = temp.next;
+            temp.next = null;
+            return temp;
+        }
+    }
+
+    // How to search element in the singly linked list
+    public boolean searchData(int searchKey){
+        ListNode current = head;
+        while(current!=null){
+            if (current.data == searchKey){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    // Reverse the singly linked list
+
+    public ListNode reverseList(ListNode head){
+        ListNode current = head;
+        ListNode previous = null;
+        ListNode next = null;
+
+        while(current != null){
+            next = current.next; // to keep a record of which node to go to next
+            current.next = previous; // reverses the direction of the list
+            previous = current; // to go to the next node
+            current = next;
+        }
+
+        return  previous;
+    }
+
 
     public static void main(String[] args) {
         SinglyLinkedList sll = new SinglyLinkedList();
@@ -93,8 +172,11 @@ public class SinglyLinkedList{
         sll.insertFirst(7);
         sll.insertFirst(5);
         sll.insertEnd(770);
-        sll.instertPosition(1,1);
-        sll.display();
-        System.out.println(sll.length());
+        sll.insertPosition(1,1);
+        sll.display(sll.head);
+        //ListNode temp = sll.deleteNode(2);
+        ListNode reversed = sll.reverseList(sll.head);
+        //System.out.println(sll.searchData(13));
+        sll.display(reversed);
     }
 }
