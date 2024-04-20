c185e35fd512ab685b0dadb291dcbf93e6cdc6ce
wanjiku <wanjikukangangi@gmail.com>
2024-04-20 11:16:26 +0300

    merge sort

--- Sort/mergeSort.java
+++ Sort/mergeSort.java
@@ -1,7 +1,54 @@
 public class MergeSort{
 
+    public int[] sort(int[] arr, int[] temp, int low, int high){
+        if (low < high){
+            int mid = low + (high - low)/2 ;
+            sort(arr, temp, low, mid);
+            sort(arr, temp, mid+1, high);
+            merge(arr, temp, low, mid, high);
+        }
+        return arr;
+    }
+
+    private void merge(int[] arr, int[] temp, int low, int mid, int high){
+        for(int i = low; i <= high; i++){
+            temp[i] = arr[i];
+        }
+        int i = low; // traverse left array,
+        int j = mid + 1;
+        int k = low;
+
+        while(i <= mid && j <= high){
+            if( temp[i] <= temp[j]){
+                arr[k] = temp[i];
+                i++;
+            } else{
+                arr[k] =temp[j];
+                j++;
+            }
+            k++;
+        }
+        while( i <= mid){
+            arr[k] = temp[i];
+            i++;
+            k++;
+        }
+    }
+
+
+
     public static void main(String[] args) {
         MergeSort ms = new MergeSort();
-
+        int[] arr = {9, 5, 2, 4, 3};
+        for( int i =0; i < arr.length; i++){
+            System.out.print(arr[i] + ", ");
+        }
+        System.out.println(" ");
+        int[] sorted = ms.sort(arr, new int[arr.length], 0, arr.length-1);
+        for( int i =0; i < sorted.length; i++){
+            System.out.print(sorted[i] + ", ");
+        }
+        System.out.println(" ");
     }
 }
+
